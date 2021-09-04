3fc0c51d2eadaf6eb162d8c9393f69dae6ae9ab3
Sevann Radhak Triztan <striztan@dacgroup.com>
2021-09-04 19:35:55 -0300

    scheduled test

---
+++ Azuretodo.Test/Tests/ScheduledFunctionTest.cs
@@ -0,0 +1,24 @@
+﻿using Azuretodo.Functions.Functions;
+using Azuretodo.Test.Helpers;
+using System;
+using Xunit;
+
+namespace Azuretodo.Test.Tests
+{
+    public class ScheduledFunctionTest
+    {
+        [Fact]
+        public void ScheduledFunction_Should_Log_Messgae()
+        {
+            // Arrange
+            MockCloudTableTodos mockTodos = new MockCloudTableTodos(new Uri("http://127.0.0.1:10002/devstoreaccount1/reports"));
+            ListLogger logger = (ListLogger)TestFactory.CreateLogger(LoggerTypes.List);
+            // Act
+            ScheduledFunction.Run(null, mockTodos, logger);
+            string message = logger.Logs[0];
+
+            // Assert
+            Assert.Contains("Deleting completed", message);
+        }
+    }
+}
