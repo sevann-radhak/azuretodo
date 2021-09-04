bb33c995c5fa0a3a7052b40670d8f2c6619100f7
Sevann Radhak Triztan <striztan@dacgroup.com>
2021-09-04 16:30:42 -0300

    deleting trigger

---
+++ Azuretodo.Functions/Functions/ScheduledFunction.cs
@@ -0,0 +1,37 @@
+using Azuretodo.Functions.Entities;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Azuretodo.Functions.Functions
+{
+    public static class ScheduledFunction
+    {
+        [FunctionName("ScheduledFunction")]
+        public static async Task Run(
+            [TimerTrigger("0 */2 * * * *")] TimerInfo myTimer,
+            [Table("todo", Connection = "AzureWebJobsStorage")] CloudTable todoTable,
+            ILogger log)
+        {
+            log.LogInformation($"Deleting completed Tasks trigger function executed at: {DateTime.Now}");
+
+            string filter = TableQuery.GenerateFilterConditionForBool("IsCompleted", QueryComparisons.Equal, true);
+            TableQuery<TodoEntity> query = new TableQuery<TodoEntity>().Where(filter);
+            TableQuerySegment<TodoEntity> completedTodos = await todoTable.ExecuteQuerySegmentedAsync(query, null);
+
+            int deleted = 0;
+
+            foreach (var ct in completedTodos)
+            {
+                await todoTable.ExecuteAsync(TableOperation.Delete(ct));
+                deleted++;
+            }
+
+            log.LogInformation($"Deleted {deleted} items at: {DateTime.Now}");
+
+        }
+    }
+}
